4a43e0788918508a1121a0acaa227c68abbaa101
Pedro Flores <armandorf.05@gmail.com>
2019-09-22 15:24:00 -0700

    Initial commit for basic project structure

---
+++ website/app.js
@@ -0,0 +1,24 @@
+/* Global Variables */
+
+
+// Personal API Key for OpenWeatherMap API
+
+
+// Event listener to add function to existing HTML DOM element
+
+
+/* Function called by event listener */
+
+
+/* Function to GET Web API Data */
+
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
+
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = `${d.getMonth()}.${d.getDate()}.${d.getFullYear()}`;
