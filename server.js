31df0aa917a154b1cbf54b2b11fdb7c6ed5b046b
Pedro Flores <pflores@apttus.com>
2019-09-23 18:05:25 -0700

    Add 2 GET routes and callbacks and 1 POST route and callback

--- server.js
+++ server.js
@@ -25,3 +25,22 @@ app.listen(port, () => { // const server =
   console.log(`Listening on port ${port}`);
 });
 
+// GET routes and callback functions
+// get a specific entry
+app.get('/entry/:id', (req, res) => {
+  res.send(projectData[req.params.id]);
+});
+
+// get all entries
+app.get('/all', (req, res) => {
+  res.send(projectData);
+});
+
+// POST routes and callback functions
+app.post('/add', (req, res) => {
+  // add data to object
+  const { body } = req;
+  projectData[body.id] = body;
+  // return most recent entry
+  res.send(projectData[body.id]);
+});
