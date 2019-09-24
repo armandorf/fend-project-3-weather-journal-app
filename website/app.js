391f7e5efade6e81657e8f3d0fcadc11155f396c
Pedro Flores <pflores@apttus.com>
2019-09-23 18:16:31 -0700

    Add function that calls OpenWeatherMap API to retrieve current temperature by zip code

--- website/app.js
+++ website/app.js
@@ -6,7 +6,25 @@ const KEY_PARAM = `&key=&APPID=${API_KEY}`;
 // global variables
 let counterId = 1;
 
+/**
+ * Gets weather data from OpenWeatherMap through an API for a given zip code.
+ * @return
+ * @param zipcode
+ * @returns {Promise<any>}
+ */
+const getWeatherData = async function (zipcode = 94040) {
+  try {
+    // construct URL
+    const url = `${WEATHER_API_BASE_URL}zip=${zipcode},us${KEY_PARAM}`;
 
+    // call weather API
+    const res = await fetch(url);
+    const json = await res.json();
+    return json;
+  } catch (err) {
+    console.log(`An error occurred while retrieving weather data: ${err}`);
+  }
+};
 
 // Event listener to add function to existing HTML DOM element
 
